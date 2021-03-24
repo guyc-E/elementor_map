8b65bb86d059763c260a467a029c26986921b7fc
Amihay Gonen <amihayg@elementor.com>
2021-03-24 20:18:24 +0200

    aa

--- backend/main.py
+++ backend/main.py
@@ -25,11 +25,11 @@ def counter():
     return '8,356,865'
 
 
-@app.route('/data')
-def data():
-    data = get_data()
-    geo_data = result_to_geo(data)
-    return geo_data
+# @app.route('/data')
+# def data():
+#     data = get_data()
+#     geo_data = result_to_geo(data)
+#     return geo_data
 
 
 if __name__ == '__main__':
